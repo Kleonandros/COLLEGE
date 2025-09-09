5790444349a4d78655b44be906c781c8ede8c7c8
Kleonandros <kleonandros@gmail.com>
2025-09-09 09:53:52 +0530

    additional learning

---
+++ python/fib_series.py
@@ -0,0 +1,10 @@
+def fib(n):   
+    """Print a Fibonacci series less than n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+choice = int(input("Enter Number Limit : "))
+fib(choice)
